b2608870ae23279eed888326e33bc85eee2f72cb
zemae <qwe123>
2024-05-23 23:53:58 +0300

    добавлено тестирования транзакций

--- client-api/src/test/java/ru/effective/clientapi/TransferControllerTest.java
+++ client-api/src/test/java/ru/effective/clientapi/TransferControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.transaction.annotation.Transactional;
+import redis.clients.jedis.Jedis;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -28,7 +29,7 @@ public class TransferControllerTest {
 
     @Test
     @Sql("/sql/users-test.sql")
-    void transferMoneyFromUsername_ReturnsHttpStatusOk() throws Exception{
+    void transferMoneyFromUsername_ReturnsHttpStatusOk() throws Exception {
 
         var requetBuilder = MockMvcRequestBuilders.post("/transfer/test1/money-transfer")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -38,7 +39,8 @@ public class TransferControllerTest {
                             "amount": 20.0
                         }
                         """)
-                .with(SecurityMockMvcRequestPostProcessors.user("test1"));;
+                .with(SecurityMockMvcRequestPostProcessors.user("test1").password("qwe123"));
+        ;
 
 
         mockMvc.perform(requetBuilder)
@@ -46,11 +48,15 @@ public class TransferControllerTest {
                 .andExpectAll(
                         status().isOk()
                 );
+
+        try (Jedis jedis = new Jedis()) {
+            jedis.flushAll();
+        }
     }
 
     @Test
     @Sql("/sql/users-test.sql")
-    void transferMoneyUsername_ReturnsProblemDetails() throws Exception{
+    void transferMoneyUsername_ReturnsProblemDetails() throws Exception {
 
         var requetBuilder = MockMvcRequestBuilders.post("/transfer/test1/money-transfer")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -60,7 +66,7 @@ public class TransferControllerTest {
                             "amount": 600
                         }
                         """)
-                .with(SecurityMockMvcRequestPostProcessors.user("test2"));
+                .with(SecurityMockMvcRequestPostProcessors.user("test2").password("qwe123"));
 
 
         mockMvc.perform(requetBuilder)
@@ -69,9 +75,131 @@ public class TransferControllerTest {
                         status().isBadRequest(),
                         content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON),
                         content().json("""
-                        {
-                            "message": "Не достаточно средств"
-                        }""")
+                                {
+                                    "message": "Не достаточно средств"
+                                }""")
                 );
+
+        try (Jedis jedis = new Jedis()) {
+            jedis.flushAll();
+        }
+    }
+
+    @Test
+    @Sql("/sql/users-test.sql")
+    void transferMoneyFromEmail_ReturnsHttpStatusOk() throws Exception {
+
+        var requetBuilder = MockMvcRequestBuilders.post("/transfer/test1/money-transfer")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                        {
+                            "email" : "test2@mail.com",
+                            "amount": 20.0
+                        }
+                        """)
+                .with(SecurityMockMvcRequestPostProcessors.user("test1").password("qwe123"));
+        ;
+
+
+        mockMvc.perform(requetBuilder)
+                .andDo(print())
+                .andExpectAll(
+                        status().isOk()
+                );
+
+        try (Jedis jedis = new Jedis()) {
+            jedis.flushAll();
+        }
+    }
+
+    @Test
+    @Sql("/sql/users-test.sql")
+    void transferMoneyFromEmail_ReturnsProblemDetails() throws Exception {
+
+        var requetBuilder = MockMvcRequestBuilders.post("/transfer/test1/money-transfer")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                        {
+                            "email" : "test2@mail.com",
+                            "amount": 10000
+                        }
+                        """)
+                .with(SecurityMockMvcRequestPostProcessors.user("test1").password("qwe123"));
+        ;
+
+
+        mockMvc.perform(requetBuilder)
+                .andDo(print())
+                .andExpectAll(
+                        status().isBadRequest(),
+                        content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON),
+                        content().json("""
+                                {
+                                    "message": "Не достаточно средств"
+                                }""")
+                );
+
+        try (Jedis jedis = new Jedis()) {
+            jedis.flushAll();
+        }
+    }
+
+    @Test
+    @Sql("/sql/users-test.sql")
+    void transferMoneyPhoneNumber_ReturnsHttpStatusOk() throws Exception {
+
+        var requetBuilder = MockMvcRequestBuilders.post("/transfer/test1/money-transfer")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                        {
+                            "phone_number" : "89999999999",
+                            "amount": 20.0
+                        }
+                        """)
+                .with(SecurityMockMvcRequestPostProcessors.user("test1").password("qwe123"));
+        ;
+
+
+        mockMvc.perform(requetBuilder)
+                .andDo(print())
+                .andExpectAll(
+                        status().isOk()
+                );
+
+        try (Jedis jedis = new Jedis()) {
+            jedis.flushAll();
+        }
+    }
+
+    @Test
+    @Sql("/sql/users-test.sql")
+    void transferMoneyPhoneNumber_ReturnsProblemDetails() throws Exception {
+
+        var requetBuilder = MockMvcRequestBuilders.post("/transfer/test1/money-transfer")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                        {
+                            "phone_number" : "89999999999",
+                            "amount": 10000
+                        }
+                        """)
+                .with(SecurityMockMvcRequestPostProcessors.user("test1").password("qwe123"));
+        ;
+
+
+        mockMvc.perform(requetBuilder)
+                .andDo(print())
+                .andExpectAll(
+                        status().isBadRequest(),
+                        content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON),
+                        content().json("""
+                                {
+                                    "message": "Не достаточно средств"
+                                }""")
+                );
+
+        try (Jedis jedis = new Jedis()) {
+            jedis.flushAll();
+        }
     }
 }
--- client-api/src/test/resources/sql/users-test.sql
+++ client-api/src/test/resources/sql/users-test.sql
@@ -1,3 +1,7 @@
-insert into users (username, start_amount, bank_account)
-values ('test1', 100, 500),
-       ('test2', 100, 100);
+insert into users (id, username, start_amount, bank_account, password)
+values (111, 'test1', 100, 500, '$2a$10$TLOq9FGsXAJ8EoPmKu65qOQelUXUM11tpcMGLPBw18KVDVwtuTQ7C'),
+       (112, 'test2', 100, 100, '$2a$10$TLOq9FGsXAJ8EoPmKu65qOQelUXUM11tpcMGLPBw18KVDVwtuTQ7C');
+
+insert into email (email, owner_email) VALUES ('test2@mail.com', 112);
+
+insert into phone_number(number, owner_phone_number) values ('89999999999', 112)
